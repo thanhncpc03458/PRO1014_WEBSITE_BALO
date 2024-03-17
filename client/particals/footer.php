5a70e393141c0f53b8fbf07c665a7331b7b1c3a9
thanhncpc03458 <110118893+thanhncpc03458@users.noreply.github.com>
2024-03-17 11:21:45 +0700

    [client] - footer

---
+++ client/particals/footer.php
@@ -0,0 +1,52 @@
+
+</main>
+<footer id="footer" class="bg-dark text-white py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <ul class="menu-footer">
+                    <li><a href="">Tích điểm</a></li>
+                    <li><a href="">Quà tặng</a></li>
+                    <li><a href="">Lịch sử mua hàng</a></li>
+                    <li><a href="">Chính sách bảo hành</a></li>
+                    <li><a href="">Xem thêm</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <ul class="menu-footer">
+                    <li><a href="">Giới Thiệu </a></li>
+                    <li><a href="">Tuyển Dụng</a></li>
+                    <li><a href="">Góp Ý, Khiếu Nại</a></li>
+                    <li><a href="">Tìm Cửa Hàng</a></li>
+                    <li><a href="">Tìm Sản Phẩm</a></li>
+                </ul>
+            </div>
+         
+
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <ul class="menu-footer">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3930.099256655871!2d105.72256447407683!3d9.92569127431091!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a08b451d4ecc4d%3A0xb91015721d18e909!2sFPT%20Shop!5e0!3m2!1svi!2s!4v1709816513523!5m2!1svi!2s" width="100%" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </ul>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <ul class="menu-footer">
+
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+        <div class="col-12 text-center">
+        Trang web bán balo là một nền tảng thương mại điện tử chuyên cung cấp các loại balo đa dạng, từ balo du lịch đến balo học đường, balo thể thao và nhiều loại balo khác nhau. Trang web này cung cấp một trải nghiệm mua sắm trực tuyến thuận tiện và linh hoạt cho khách hàng.
+                </div>
+        </div>
+
+    </div>
+
+
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
